6cb97a013a59858e6f4866923293abb32c5de6c7
Clément <clement.ramos-lage@seniormedia.fr>
2022-10-08 22:16:02 +0200

    feat: add style

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,24 @@ import Link from 'next/link'
 import { Client } from '@notionhq/client'
 
 export default function Home({ page }) {
-  return (
+  return <>
     <Link href={page.link}>
       <a target="_blank">
-        {page.title}
+        <div className='w-screen h-screen px-6 flex items-center flex-wrap justify-center rounded-xl bg-gradient-to-r from-cyan-400 to-sky-500 text-lg text-white'>
+          {page.title}
+        </div>
       </a>
     </Link>
-  )
+  </>
 }
 
 export async function getServerSideProps() {
   const notion = new Client({ auth: process.env.NOTION_API_TOKEN })
-  const databaseId = process.env.NOTION_DATABASE
+  const pages = (await notion.databases.query({
+    database_id: process.env.NOTION_DATABASE
+  }))?.results
 
-  const pages = (await notion.databases.query({ database_id: databaseId }))?.results
   const p = pages[Math.floor(Math.random() * pages.length)]
-
   return {
     props: {
       page: {
